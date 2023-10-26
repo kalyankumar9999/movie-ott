3a63878c7815714caac5ca28c5f9131ba935efc1
Kalyan Kumar <kalyan.kumar@codecraft.co.in>
2023-10-26 09:38:53 +0530

    fix:resolved commented changesfor inputcomponent

--- src/components/inputComponent/inputcomponent.tsx
+++ src/components/inputComponent/inputcomponent.tsx
@@ -3,17 +3,21 @@ interface InputComponentProps {
   customPlaceHolder: string;
   customLabel: string;
   customInputType: string;
+  error: string;
 }
 const InputComponent = (props: InputComponentProps) => {
-  const { customPlaceHolder, customLabel, customInputType } = props;
+  const { customPlaceHolder, customLabel, customInputType, error } = props;
   return (
     <div className={styles.inputFieldContainer}>
       <label className={styles.inputLabel}>{customLabel}</label>
       <input
         type={customInputType}
         placeholder={customPlaceHolder}
-        className={styles.inputField}
+        className={`${styles.inputField} ${
+          error ? styles.inputFieldError : ""
+        }`}
       />
+      {error && <div className={styles.inputFieldErrorText}>{error}</div>}
     </div>
   );
 };
